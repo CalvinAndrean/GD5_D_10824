020de295b6c789ae5055b2c1d7712fd48ace9ba7
CalvinAndrean <calvinandrean456@gmail.com>
2022-09-20 10:00:15 +0700

    Initial commit

---
+++ app/src/main/java/com/gd5/gdroom_d_10824/room/Note.kt
@@ -0,0 +1,11 @@
+package com.gd5.gdroom_d_10824.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String)
